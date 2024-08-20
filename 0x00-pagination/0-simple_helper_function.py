5198c9a7011bbc11e60c41826214919535e3ca20
thatguykwameC <thatguykwamec@gmail.com>
2024-08-19 20:20:22 -0400

    A helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Simple helper function """
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """ 
+    Calcs start and end index for pagination
+
+    Args:
+        page: current page
+        page_size: number of items per page
+
+    Returns:
+        tuple of start and end index
+    """
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
