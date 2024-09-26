c53937f086d76a779697ad577aadecff52b6de12
thatguykwameC <thatguykwamec@gmail.com>
2024-09-26 14:37:12 -0400

    redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) =>
+  console.log(`Redis client not connected to the server: ${err.message}`)
+);
+
+client.on('connect', () => console.log('Redis client connected to the server'));
